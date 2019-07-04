a97c62311cdb0783a1a539f848b8ed7b200fb94a
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-04 14:45:25 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,11 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+
+export default class DigitalClicker extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      timesClicked:0
+    }
+  }
+}
