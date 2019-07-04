47d45eccb5083d6ee3db287b7940a166d199f368
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-04 15:31:38 +0000

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,18 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+
+export default class DigitalClicker extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      timesClicked:0
+    }
+  }
+
+  clickHandler = () => {
+    this.setState({timesClicked:this.state.timesClicked + 1})
+  }
+  render(){
+    return(<button onClick={this.clickHandler}>{this.state.timesClicked}</button>)
+  }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,31 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+export default class YouTubeDebugger extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    }
+  }
+
+  bitHandler = () => {
+    this.setState({settings:{...this.state.settings,bitrate:12}});
+  }
+
+  resolutionHandler = () => {
+    this.setState({settings:{...this.state.settings,video:{...this.state.settings.video,resolution:'720p'}}});
+  }
+
+  render(){
+    return(<div><button className='bitrate' onClick={this.bitHandler}></button>
+          <button className='resolution' onClick={this.resolutionHandler}></button></div>);
+  }
+}
