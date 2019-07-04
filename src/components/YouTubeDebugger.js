e77ef3437c4788d3f4a07ae38faad41f75a483df
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-04 14:54:04 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,18 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+export default class YouTubeDebugger extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      timesClicked:0
+    }
+  }
+
+  clickHandler = () => {
+    this.setState({timesClicked:timesClicked + 1})
+  }
+  render(){
+    return(<button onClick={this.clickHandler}></button>)
+  }
+}
